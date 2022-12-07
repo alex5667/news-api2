381284e859e17285eae82d83296ba71a24c6cad6
alex5667 <alex56671111@gmail.com>
2022-12-07 23:58:48 +0200

    feat: add  setting in  eslint

--- news-api/.eslintrc.cjs
+++ news-api/.eslintrc.cjs
@@ -3,4 +3,13 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "warn",
+    '@typescript-eslint/no-var-requires': 0,
+  },
+  "env": {
+    "browser": true,
+    "amd": true,
+    "node": true
+  },
 };
