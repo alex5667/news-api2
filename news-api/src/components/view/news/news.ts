4df5525b094371afec274b86bc3af764b4a44c1b
alex5667 <alex56671111@gmail.com>
2022-12-11 01:22:07 +0200

    feat: add news ts

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -1,20 +1,38 @@
 import './news.css';
+export interface Data {
+    "source": {
+        "id": number,
+        "name": string
+    },
+    "author": string,
+    "title": string,
+    "description": string,
+    "url": string,
+    "urlToImage": string,
+    "publishedAt": string,
+    "content": string,
+}
+
+
 
 class News {
-    draw(data) {
+    draw(data: readonly Data[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
 
         news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            const newsClone = newsItemTemp.content.cloneNode(true) as Element;
+            const newsItem = newsClone.querySelector('.news__item')
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
-            })`;
+            if (idx % 2) {
+                newsItem && newsItem.classList.add('alt');
+            }
+
+            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'
+                })`;
             newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
             newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
                 .slice(0, 10)
