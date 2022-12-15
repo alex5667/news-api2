cd05029dc61f8bc9c43fbf4c74b0c9003a6c34f5
alex5667 <alex56671111@gmail.com>
2022-12-15 16:14:21 +0200

    fix: change api

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '783925b410aa41ae83f3d039f18d4b75', // получите свой ключ https://newsapi.org/
         });
     }
